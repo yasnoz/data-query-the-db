64db716a85a47da3d4ad00a85c476fa7a4eb072e
Yasin Oz <yasnoz@gmail.com>
2024-07-12 15:48:12 +0200

    sql advance lesson, 10/10 passed, second exercise ecommersdatabase

--- queries.py
+++ queries.py
@@ -2,15 +2,37 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query ="""SELECT *
+            FROM Orders o
+            """
+    results = db.execute(query)
+    results = results.fetchall()
+    # results in a list (rows) of lists (columns)
+    # Then you'll need to return something.
+    return results
+
+
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between
     # date_from and date_to (excluding date_from and including date_to)
-    pass  # YOUR CODE HERE
+    query ="""SELECT *
+           FROM Orders o
+           WHERE o.OrderDate > ? AND o.OrderDate <= ?
+           ORDER BY OrderDate ASC"""
+    results = db.execute(query, (date_from, date_to))
+    results = results.fetchall()
+    return results
+
+
 
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    query ="""SELECT *, julianday(ShippedDate) - julianday(OrderDate) AS waiting_time
+           FROM Orders o
+           ORDER BY waiting_time ASC"""
+    results = db.execute(query)
+    results = results.fetchall()
+    return results
